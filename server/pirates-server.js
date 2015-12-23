f8c54fe2496f734914be99c3b9767a27f12497f9
William Hiver <william.hiver@free.fr>
2015-12-23 12:28:54 +0100

    Import Express and Socket.io, serve the files & begin the Socket.io communications

--- server/pirates-server.js
+++ server/pirates-server.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /******************************************************************************
     This file is part of PIRATES.
 
@@ -14,3 +15,39 @@
     You should have received a copy of the GNU General Public License
     along with PIRATES.  If not, see <http://www.gnu.org/licenses/>.
 *******************************************************************************/
+
+//---------------------- Import required modules
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+//------------------- Serve the HTML/Js client files
+app.get('/', function (req, res) {
+    res.setHeader('Content-type', 'text/html');
+    res.sendFile(__dirname + '/../index.html');
+})
+
+.get('/app.min.js', function (req, res) {
+    res.setHeader('Content-type', 'text/javascript');
+    res.sendFile(__dirname + '/../js/app.min.js');
+});
+
+//-------------- Handle the client-server communication
+io.on('connection', function (socket) {
+    
+    // Allow a player to join the game and set his pseudo
+    socket.on('join', function (pseudo) {
+        socket.pseudo = pseudo;
+        console.log('Player ' + pseudo + ' joined the game.');
+        socket.broadcast.emit('joined', pseudo);
+        // TODO Send the initial position of the player
+    });
+    
+    // TODO Update players stats (position, items & life)
+});
+
+//-------------------- Initialize the connection
+server.listen(80, function() {
+    console.log('listening on *:80');
+});
