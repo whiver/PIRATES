86ed12c9ee3a16f1ae1a46f797a9ecc2fe04020f
Adrien Coppola <Adrien Coppola>
2016-02-06 18:11:41 +0100

    mainPlayer added (+ move animations)

---
+++ js/entities/Player.js
@@ -0,0 +1,124 @@
+/******************************************************************************
+This file is part of PIRATES.
+
+PIRATES is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+PIRATES is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with PIRATES.  If not, see <http://www.gnu.org/licenses/>.
+*******************************************************************************/
+
+/**
+* Player Entity
+*/
+game.Player = me.Entity.extend({
+
+  /**
+  * constructor
+  */
+  init:function (x, y, settings) {
+    // call the constructor
+    this._super(me.Entity, 'init', [x, y , settings]);
+
+    // set the default horizontal & vertical speed (accel vector)
+    this.body.setVelocity(2, 2);
+
+    this.body.gravity = 0;
+
+    // set the display to follow our position on both axis
+    me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+
+    // ensure the player is updated even when outside of the viewport
+    this.alwaysUpdate = true;
+
+    // define a basic walking animation (using all frames)
+    this.renderable.addAnimation("downWalk",  [0, 1, 2]);
+    this.renderable.addAnimation("upWalk",  [36, 37, 38]);
+    this.renderable.addAnimation("sideWalk",  [24, 25, 26]);
+
+    // define a standing animation (using the first frame)
+    this.renderable.addAnimation("stand",  [1]);
+    // set the standing animation as default
+    this.renderable.setCurrentAnimation("stand");
+  },
+
+  /**
+  * update the entity
+  */
+  update : function (dt) {
+    //Go left
+    if (me.input.isKeyPressed('left')) {
+      // flip the sprite on horizontal axis
+      this.renderable.flipX(true);
+      // update the entity velocity
+      this.body.vel.x -= this.body.accel.x * me.timer.tick;
+      // change to the walking animation
+      if (!this.renderable.isCurrentAnimation("sideWalk")) {
+        this.renderable.setCurrentAnimation("sideWalk");
+      }
+    //Go right
+    } else if (me.input.isKeyPressed('right')) {
+      // unflip the sprite
+      this.renderable.flipX(false);
+      // update the entity velocity
+      this.body.vel.x += this.body.accel.x * me.timer.tick;
+      // change to the walking animation
+      if (!this.renderable.isCurrentAnimation("sideWalk")) {
+        this.renderable.setCurrentAnimation("sideWalk");
+      }
+    }
+    //Doesn't move on x axis
+    else {
+      this.body.vel.x = 0;
+    }
+
+    //Go up
+    if (me.input.isKeyPressed('up')){
+      this.body.vel.y -= this.body.accel.y * me.timer.tick;
+      // change to the walking animation
+      if (!this.renderable.isCurrentAnimation("upWalk") && !me.input.isKeyPressed('right') && !me.input.isKeyPressed('left')) {
+        this.renderable.setCurrentAnimation("upWalk");
+      }
+    //Go down
+    } else if (me.input.isKeyPressed('down')){
+      this.body.vel.y += this.body.accel.y * me.timer.tick;
+      // change to the walking animation
+      if (!this.renderable.isCurrentAnimation("downWalk") && !me.input.isKeyPressed('right') && !me.input.isKeyPressed('left')) {
+        this.renderable.setCurrentAnimation("downWalk");
+      }
+    //Doesn't move on y axis
+    } else {
+      this.body.vel.y = 0;
+    }
+
+    //Stand animation
+    if(!me.input.isKeyPressed('up') && !me.input.isKeyPressed('down') && !me.input.isKeyPressed('left') && !me.input.isKeyPressed('right')){
+      this.renderable.setCurrentAnimation("stand");
+    }
+
+    // apply physics to the body (this moves the entity)
+    this.body.update(dt);
+
+    // handle collisions against other shapes
+    me.collision.check(this);
+
+    // return true if we moved or if the renderable was updated
+    return (this._super(me.Entity, 'update', [dt]) || this.body.vel.x !== 0 || this.body.vel.y !== 0);
+  },
+
+  /**
+  * colision handler
+  * (called when colliding with other objects)
+  */
+  onCollision : function (response, other) {
+    // Make all other objects solid
+    return true;
+  }
+});
--- js/game.js
+++ js/game.js
@@ -1,67 +1,73 @@
 /******************************************************************************
-    This file is part of PIRATES.
+This file is part of PIRATES.
 
-    PIRATES is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+PIRATES is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    PIRATES is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+PIRATES is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with PIRATES.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with PIRATES.  If not, see <http://www.gnu.org/licenses/>.
 *******************************************************************************/
 
 /* Game namespace */
 var game = {
 
-    // an object where to store game information
-    data : {
-        // score
-        score : 0
-    },
+  // an object where to store game information
+  data : {
+    // score
+    score : 0
+  },
 
 
-    // Run on page load.
-    "onload" : function () {
-        // Initialize the video.
-        if (!me.video.init(640, 480, {wrapper : "screen", scale : "auto"})) {
-            alert("Your browser does not support HTML5 canvas.");
-            return;
-        }
-
-        // add "#debug" to the URL to enable the debug Panel
-        if (me.game.HASH.debug === true) {
-            window.onReady(function () {
-                me.plugin.register.defer(this, me.debug.Panel, "debug", me.input.KEY.V);
-            });
-        }
-
-        // Initialize the audio.
-        me.audio.init("mp3,ogg");
-
-        // Set a callback to run when loading is complete.
-        me.loader.onload = this.loaded.bind(this);
-
-        // Load the resources.
-        me.loader.preload(game.resources);
-
-        // Initialize melonJS and display a loading screen.
-        me.state.change(me.state.LOADING);
-    },
-
-    // Run on game resources loaded.
-    "loaded" : function () {
-        me.state.set(me.state.MENU, new game.TitleScreen());
-        me.state.set(me.state.PLAY, new game.PlayScreen());
-
-        // add our player entity in the entity pool
-        me.pool.register("mainPlayer", game.PlayerEntity);
-
-        // Start the game.
-        me.state.change(me.state.PLAY);
+  // Run on page load.
+  "onload" : function () {
+    // Initialize the video.
+    if (!me.video.init(640, 480, {wrapper : "screen", scale : "auto"})) {
+      alert("Your browser does not support HTML5 canvas.");
+      return;
     }
+
+    // add "#debug" to the URL to enable the debug Panel
+    if (me.game.HASH.debug === true) {
+      window.onReady(function () {
+        me.plugin.register.defer(this, me.debug.Panel, "debug", me.input.KEY.V);
+      });
+    }
+
+    // Initialize the audio.
+    me.audio.init("mp3,ogg");
+
+    // Set a callback to run when loading is complete.
+    me.loader.onload = this.loaded.bind(this);
+
+    // Load the resources.
+    me.loader.preload(game.resources);
+
+    // Initialize melonJS and display a loading screen.
+    me.state.change(me.state.LOADING);
+  },
+
+  // Run on game resources loaded.
+  "loaded" : function () {
+    me.state.set(me.state.MENU, new game.TitleScreen());
+    me.state.set(me.state.PLAY, new game.PlayScreen());
+
+    // add our player entity in the entity pool
+    me.pool.register("mainPlayer", game.Player);
+
+    //Key mappings
+    me.input.bindKey(me.input.KEY.LEFT,  "left");
+    me.input.bindKey(me.input.KEY.RIGHT, "right");
+    me.input.bindKey(me.input.KEY.UP,  "up");
+    me.input.bindKey(me.input.KEY.DOWN, "down");
+
+    // Start the game.
+    me.state.change(me.state.PLAY);
+  }
 };
