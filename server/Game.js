e2cc0d8bbba5d2ffecbf9a00290dfad93a0445f6
Adri-7 <adrien.coppola@gmail.com>
2016-05-04 00:10:25 +0200

    respawn points added

--- server/Game.js
+++ server/Game.js
@@ -28,6 +28,14 @@
      */
     constructor(){
       this.players = {};
+      this.respawnPoints = [];
+
+      this.respawnPoints[0] = {x: '2048', y: '864'};
+      this.respawnPoints[1] = {x: '640', y: '704'};
+      this.respawnPoints[2] = {x: '608', y: '2048'};
+      this.respawnPoints[3] = {x: '1728', y: '1920'};
+      this.respawnPoints[4] = {x: '2112', y: '352'};
+      this.respawnPoints[5] = {x: '1280', y: '1280'};
     }
 
     /**
@@ -39,24 +47,36 @@
      */
     AddPlayer(pseudo){
       var id = -1;
-	  
-	  // Check if pseudo is already defined
-	  var listPlayers = this.players;
-	  for(var i in listPlayers) {
-		  var player = listPlayers[i];
-		  if(pseudo == player.pseudo)
-		  { 
-			return id; 
-		  }
-	  }
 
-      //TODO make a system to locate the players in differents parts of the map
-      Game.NbPlayers++;
+  	  // Check if pseudo is already defined
+  	  var listPlayers = this.players;
+  	  for(var i in listPlayers) {
+  		  var player = listPlayers[i];
+  		  if(pseudo == player.pseudo)
+  		  {
+          return id;
+  		  }
+  	  }
 
       id = Game.NbPlayers;
-      this.players[id] = new Entities.Player(pseudo, id, 400, 400);
+      var x = this.respawnPoints[id].x;
+      var y = this.respawnPoints[id].y;
+
+      this.players[id] = new Entities.Player(pseudo, id, x, y);
+
+      Game.NbPlayers++;
 
       return id;
+    }
+
+    /**
+     * RemovePlayer - Remove a player from the listPlayers
+     *
+     * @param {int} id the player to remove
+     */
+    RemovePlayer(id){
+      if(this.player[id] !== undefined)
+        delete this.player[id];
     }
 
     /**
